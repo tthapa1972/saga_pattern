fa9780e933e27f386433039f9920f455cc5650b1
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-05-26 15:11:07 +0545

    saga-pattern : product-service

---
+++ saga-pattern/order-service/src/main/java/com/microservices/saga/order/config/OrderStatusUpdateHandler.java
@@ -0,0 +1,46 @@
+package com.microservices.saga.order.config;
+
+import com.microservices.saga.config.dto.OrderRequestDto;
+import com.microservices.saga.config.event.OrderStatus;
+import com.microservices.saga.config.event.PaymentStatus;
+import com.microservices.saga.order.entity.PurchaseOrder;
+import com.microservices.saga.order.repository.OrderRepository;
+import com.microservices.saga.order.service.OrderStatusPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class OrderStatusUpdateHandler {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Autowired
+    private OrderStatusPublisher publisher;
+
+    @Transactional
+    public void updateOrder(int id, Consumer<PurchaseOrder> consumer) {
+        repository.findById(id).ifPresent(consumer.andThen(this::updateOrder));
+    }
+
+    private void updateOrder(PurchaseOrder purchaseOrder) {
+        boolean isPaymentComplete = PaymentStatus.PAYMENT_SUCCESS.equals(purchaseOrder.getPaymentStatus());
+        OrderStatus orderStatus = isPaymentComplete ? OrderStatus.ORDER_COMPLETED : OrderStatus.ORDER_CANCELLED;
+        purchaseOrder.setOrderStatus(orderStatus);
+        if (!isPaymentComplete) {
+            publisher.publishOrderEvent(convertEntityToDto(purchaseOrder), orderStatus);
+        }
+    }
+
+    public OrderRequestDto convertEntityToDto(PurchaseOrder purchaseOrder) {
+        OrderRequestDto orderRequestDto = new OrderRequestDto();
+        orderRequestDto.setOrderId(purchaseOrder.getId());
+        orderRequestDto.setUserId(purchaseOrder.getUserId());
+        orderRequestDto.setAmount(purchaseOrder.getPrice());
+        orderRequestDto.setProductId(purchaseOrder.getProductId());
+        return orderRequestDto;
+    }
+}
